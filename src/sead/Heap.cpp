f262831415eef6f1a637c8bb2b257ce8033e89aa
Luminyx <27lumi@protonmail.com>
2026-02-15 19:35:52 -0500

    Restore heap functions

---
+++ src/sead/Heap.cpp
@@ -0,0 +1,31 @@
+#include <telkin/Telkin.h>
+#include <heap/seadUnitHeap.h>
+#include <heap/seadFrameHeap.h>
+#include <heap/seadSeparateHeap.h>
+#include <heap/seadExpHeap.h>
+#include <player/PlayerObject.h>
+
+const void* sead::UnitHeap::getStartAddress() const {
+    return mStart;
+}
+tPointer(0x101914A4, sead::UnitHeap::getStartAddress, false);
+
+const void* sead::SeparateHeap::getStartAddress() const {
+    return mStart;
+}
+tPointer(0x101A4658, sead::SeparateHeap::getStartAddress, false);
+
+size_t sead::ExpHeap::getSize() const {
+    return mSize;
+}
+tPointer(0x10191094, sead::ExpHeap::getSize, false);
+
+size_t sead::FrameHeap::getSize() const {
+    return mSize;
+}
+tPointer(0x101911C4, sead::FrameHeap::getSize, false);
+
+size_t sead::UnitHeap::getSize() const {
+    return mSize;
+}
+tPointer(0x101914B4, sead::UnitHeap::getSize, false);
