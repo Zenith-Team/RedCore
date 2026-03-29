ff3596a1d6c62d228f17eacbcf4990e67ae5971c
Luminyx <27lumi@protonmail.com>
2026-03-29 17:16:28 -0400

    Add PCH

---
+++ src/PCH.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <limits>
+#include <new>
+#include <source_location>
+#include <span>
+#include <type_traits>
+#include <utility>
+
+#include <cafe.h>
+
+#include <heap/seadHeap.h>
+#include <math/seadMathCalcCommon.h>
+#include <math/seadVector.h>
+#include <random/seadGlobalRandom.h>
+
+#define PROFILE_INFO_AS_NAMESPACE
+#include <audio/GameAudio.h>
+#include <boss/Boss.h>
+#include <course_select/CourseSelectTask.h>
+#include <effect/EffectCreateUtil.h>
+#include <enemy/Enemy.h>
+#include <enemy/Kuribo.h>
+#include <game/CourseTask.h>
+#include <graphics/JointBlendModel.h>
+#include <player/PlayerObject.h>
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include <imgui/imgui.h>
