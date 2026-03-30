e03135b2485fa1adb450aabb27216d9687ef2a38
Luminyx <27lumi@protonmail.com>
2026-03-29 21:30:18 -0400

    Runtime: Use more impls from TK

--- src/runtime/IO.cpp
+++ src/runtime/IO.cpp
@@ -1,14 +1,3 @@
-#include <stdarg.h>
-#include <limits.h>
-#include <cafe/os.h>
-
 #define TK_IMPL_PRINTF
+#define TK_IMPL_SPRINTF
 #include <telkin/Runtime.h>
-
-extern "C" int sprintf(char* buffer, const char* format, ...) {
-    __va_list va;
-    va_start(va, format);
-    const int ret = vsnprintf(buffer, INT_MAX, format, va);
-    va_end(va);
-    return ret;
-}
--- src/runtime/Math.cpp
+++ src/runtime/Math.cpp
@@ -1,34 +1,7 @@
-#include <cmath>
-
 #define TK_IMPL_FABS
 #define TK_IMPL_FABSF
 #define TK_IMPL_ATOF
+#define TK_IMPL_FLOOR
+#define TK_IMPL_POW
+#define TK_IMPL_LOG
 #include <telkin/Runtime.h>
-
-extern "C" double floor(double x) {
-    return floorf(x);
-}
-
-extern "C" double ceil(double x) {
-    return ceilf(x);
-}
-
-extern "C" double sin(double x) {
-    return sinf(x);
-}
-
-extern "C" double pow(double x, double y) {
-    return powf(x, y);
-}
-
-extern "C" double log(double x) {
-    return logf(x);
-}
-
-extern "C" double cos(double x) {
-    return cosf(x);
-}
-
-extern "C" double atan2(double y, double x) {
-    return atan2f(y, x);
-}
