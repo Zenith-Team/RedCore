f3d4caa810e4f3cba4001843e85e14a7b48fc4e3
Luminyx <27lumi@protonmail.com>
2026-04-04 21:11:50 -0400

    sead: Add Assert.cpp

---
+++ src/sead/Assert.cpp
@@ -0,0 +1,31 @@
+#include <basis/seadAssert.h>
+#include <telkin/Print.h>
+#include <cstdarg>
+#include <cstdio>
+
+void sead::system::HaltWithDetail(const char* pos, s32 line, const char* format, ...) {
+    char buffer[2048];
+
+    int offset = __os_snprintf(buffer, sizeof(buffer), "[%s:%d] ", tk::internal::basename(pos), line);
+
+    if (offset >= 0 && offset < static_cast<int>(sizeof(buffer))) {
+        va_list args;
+        va_start(args, format);
+        std::vsnprintf(buffer + offset, sizeof(buffer) - offset, format, args);
+        va_end(args);
+    } else {
+        OSReport("HaltWithDetail buffer overflow attempting to print prefix.\n");
+    }
+
+    OSReport("%s\n", buffer);
+    OSFatal(buffer);
+}
+
+void sead::system::HaltWithDetailNoFormat(const char* pos, s32 line, const char* str) {
+    char buffer[2048];
+    
+    __os_snprintf(buffer, sizeof(buffer), "[%s:%d] %s", tk::internal::basename(pos), line, str);
+    
+    OSReport("%s\n", buffer);
+    OSFatal(buffer);
+}
