5811b1258a179894ae932bcc4a1334f33826cf3f
Luminyx <27lumi@protonmail.com>
2026-04-23 04:41:17 -0400

    sead/Assert: Tidy code

--- src/sead/Assert.cpp
+++ src/sead/Assert.cpp
@@ -3,11 +3,10 @@
 #include <cstdarg>
 #include <cstdio>
 
-void sead::system::HaltWithDetail(const char* pos, s32 line, const char* format, ...) {
+void sead::system::HaltWithDetail(const char* pos, const s32 line, const char* format, ...) {
     char buffer[2048];
 
     int offset = __os_snprintf(buffer, sizeof(buffer), "[%s:%d] ", tk::internal::basename(pos), line);
-
     if (offset >= 0 && offset < static_cast<int>(sizeof(buffer))) {
         va_list args;
         va_start(args, format);
@@ -21,7 +20,7 @@ void sead::system::HaltWithDetail(const char* pos, s32 line, const char* format,
     OSFatal(buffer);
 }
 
-void sead::system::HaltWithDetailNoFormat(const char* pos, s32 line, const char* str) {
+void sead::system::HaltWithDetailNoFormat(const char* pos, const s32 line, const char* str) {
     char buffer[2048];
     
     __os_snprintf(buffer, sizeof(buffer), "[%s:%d] %s", tk::internal::basename(pos), line, str);
