246ecfd0d83df450b3d13c7e7c35d36168900009
Luminyx <27lumi@protonmail.com>
2026-05-17 11:46:31 -0400

    safety/HeapAlloc: Optimize call guard

--- src/safety/HeapAlloc.cpp
+++ src/safety/HeapAlloc.cpp
@@ -5,8 +5,11 @@
 namespace red {
 
 TaskPrepareEvent::Listener<TaskPrepareEvent::Stage::Before> InstallAllocFailedCallback([](TaskPrepareEvent& e) {
-    if (e.getTask()->getName() != "RootTask")
+    static bool installed = false;
+    if (installed)
         return;
+    
+    installed = true;
 
     static auto deleg = sead::FunctionDelegateCreator<const sead::HeapMgr::AllocFailedCallbackArg*>([](const sead::HeapMgr::AllocFailedCallbackArg* arg) {
         tk::fatal("Failed to allocate 0x%X bytes (align 0x%X) from heap: %s (had free size 0x%X)", arg->request_size, arg->request_alignment, arg->heap->getName().cstr(), arg->heap->getFreeSize());
