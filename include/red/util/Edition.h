7de027215e3b9e407e7a723f479cfb5622676c59
Luminyx <27lumi@protonmail.com>
2026-07-31 15:04:43 -0400

    util: Add edition macro

---
+++ include/red/util/Edition.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#if __TITLEID__ == 0x00050000'1014B700 || __TITLEID__ == 0x00050000'1014B800
+#define __BUNDLE__
+#endif
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,15 @@
 #include <telkin/Print.h>
 #include <telkin/Hooks.h>
+#include <layout_sys/TextBox.h>
+#include <red/util/Edition.h>
 
 void main() {
     tk::println("Welcome to REDCORE");
 }
 
-#include <layout_sys/TextBox.h>
-
 namespace red {
 
-void titleVer(TextBox* textBox)
-{
+void titleVer(TextBox* textBox) {
     textBox->SetString(L"Red." MOD_VERSION);
     textBox->SetVtxColor(0, nw::ut::Color8(255, 255, 255, 255));
     textBox->SetVtxColor(2, nw::ut::Color8(255, 0, 0, 255));
@@ -18,8 +17,10 @@ void titleVer(TextBox* textBox)
 
 }
 
+#ifndef __BUNDLE__
 tBranch(0x02668138, red::titleVer, tk::BranchType::bl); // Title::create
 tBranch(0x026681A0, red::titleVer, tk::BranchType::bl); // Title::create
 
 tBranch(0x02667FC4, red::titleVer, tk::BranchType::bl); // Title::create
 tBranch(0x0266802C, red::titleVer, tk::BranchType::bl); // Title::create
+#endif
