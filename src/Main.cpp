ca935b0ed777590d039b5ec4272d38831657308a
STUPID Modder <stupidmoddersoup@gmail.com>
2026-07-31 13:35:21 -0300

    Main: TitleScreen version !

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,25 @@
 #include <telkin/Print.h>
+#include <telkin/Hooks.h>
 
 void main() {
     tk::println("Welcome to REDCORE");
 }
+
+#include <layout_sys/TextBox.h>
+
+namespace red {
+
+void titleVer(TextBox* textBox)
+{
+    textBox->SetString(L"Red." MOD_VERSION);
+    textBox->SetVtxColor(0, nw::ut::Color8(255, 255, 255, 255));
+    textBox->SetVtxColor(2, nw::ut::Color8(255, 0, 0, 255));
+}
+
+}
+
+tBranch(0x02668138, red::titleVer, tk::BranchType::bl); // Title::create
+tBranch(0x026681A0, red::titleVer, tk::BranchType::bl); // Title::create
+
+tBranch(0x02667FC4, red::titleVer, tk::BranchType::bl); // Title::create
+tBranch(0x0266802C, red::titleVer, tk::BranchType::bl); // Title::create
