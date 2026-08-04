c8d483342e5551e8011e1b3409f600149fa0224f
Luminyx <27lumi@protonmail.com>
2026-08-04 01:44:10 -0400

    fixes/BonusTime: init in BoostRush too

--- src/fixes/BonusTime.cpp
+++ src/fixes/BonusTime.cpp
@@ -1,4 +1,5 @@
 #include <telkin/Hooks.h>
 
 // Always initialize BonusTime even outside of NSLU
+tPatchNop(0x025D4530); // Gamescene::create (this is for BoostRush)
 tPatchNop(0x025D45A0); // Gamescene::create
