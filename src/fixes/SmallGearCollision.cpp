199699b63335e603c5c21c25525e2575e4b306d9
Luminyx <27lumi@protonmail.com>
2026-08-30 17:03:33 -0400

    fixes/SmallGearCollision: use assembler for instr

--- src/fixes/SmallGearCollision.cpp
+++ src/fixes/SmallGearCollision.cpp
@@ -3,4 +3,4 @@
 using namespace tk::ppc;
 
 // Small Gear Collision Fix
-tPatch32u(0x0277ECA4, 0xC00B91F4); // Change the value read in staticinit from 32.0f to the proper 72.0f
+tPatch32u(0x0277ECA4, lfs(F::f0, R::r11, -0x6E0C)); // Change the value read in staticinit from 32.0f to the proper 72.0f
